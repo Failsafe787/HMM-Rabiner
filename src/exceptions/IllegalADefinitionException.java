05040fd00f400177fa93eadf2460f34c4b6d5817
Luca Banzato <Luca Banzato@melusine>
2018-02-13 18:42:27 +0100

    Added a missing file

---
+++ src/exceptions/IllegalADefinitionException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+public class IllegalADefinitionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8786973103220109249L;
+
+	public IllegalADefinitionException(String message) {
+		super(message);
+	}
+
+	public IllegalADefinitionException() {
+		super();
+	}
+
+}
